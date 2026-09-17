514ba6712ff68367e8f00548df39c16e3c544fbc
Zach Estrella <zachgabriel27@gmail.com>
2026-09-18 01:48:50 +0800

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,160 @@
+"use client"
+
 import Image from "next/image";
+import React from "react";
+import Webcam from "react-webcam";
 
 export default function Home() {
+  const webcamRef = React.useRef(null);
+  const [imgSrc,setImgSrc] = React.useState(null);
+  const [time,setTime] = React.useState<number|null>(null);
+
+  const startCountdown = ()  => {
+    setTime(4);
+  }
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+
+  // const generateStrip = () => {
+  //   const canvas = document.createElement("canvas");
+
+  //   canvas.width = 600;
+  //   canvas.height = 1800;
+
+  //   const ctx = canvas.getContext("2d");
+
+  //   if (!ctx) return;
+
+  //   // =========================
+  //   // Canvas background
+  //   // =========================
+
+  //   ctx.fillStyle = "#000000";
+  //   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  //   // =========================
+  //   // Layout
+  //   // =========================
+
+  //   const padding = 50;
+  //   const gap = 30;
+
+  //   const photoWidth = canvas.width - padding * 2;
+  //   const photoHeight = 350;
+
+  //   // =========================
+  //   // Photo placeholders
+  //   // =========================
+
+  //   for (let i = 0; i < 4; i++) {
+  //     const x = padding;
+  //     const y = padding + i * (photoHeight + gap);
+
+  //     // Photo background
+  //     ctx.fillStyle = "#e5e5e5";
+  //     ctx.fillRect(
+  //       x,
+  //       y,
+  //       photoWidth,
+  //       photoHeight
+  //     );
+
+  //     // Photo border
+  //     ctx.strokeStyle = "#bdbdbd";
+  //     ctx.lineWidth = 3;
+  //     ctx.strokeRect(
+  //       x,
+  //       y,
+  //       photoWidth,
+  //       photoHeight
+  //     );
+
+  //     // Placeholder text
+  //     ctx.fillStyle = "#999999";
+  //     ctx.font = "bold 24px sans-serif";
+  //     ctx.textAlign = "center";
+  //     ctx.textBaseline = "middle";
+
+  //     ctx.fillText(
+  //       `PHOTO ${i + 1}`,
+  //       canvas.width / 2,
+  //       y + photoHeight / 2
+  //     );
+  //   }
+
+  //   // =========================
+  //   // Footer
+  //   // =========================
+
+  //   ctx.fillStyle = "#333333";
+  //   ctx.font = "bold 24px sans-serif";
+  //   ctx.textAlign = "center";
+  //   ctx.textBaseline = "middle";
+
+  //   ctx.fillText(
+  //     "Zach ❤️ ___",
+  //     canvas.width / 2,
+  //     1640
+  //   );
+
+  //   ctx.font = "18px sans-serif";
+
+  //   ctx.fillText(
+  //     "August 23, 2026",
+  //     canvas.width / 2,
+  //     1680
+  //   );
+
+  //  return canvas.toDataURL("image/png");
+  // };
+
+  // const strip = generateStrip();
+
+  React.useEffect(() => {
+    if (time === null) return;
+
+    if (time === 0) {
+      capture();
+      setTime(null);
+      return;
+    }
+
+    const startTimer = setTimeout(() => {
+      setTime((prev) => prev - 1)
+    },1000)
+
+    return () => clearTimeout(startTimer);
+  },[time]) 
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="flex flex items-center justify-center gap-24">
+      <div className="relative w-3/5">
+        <Webcam
+          mirrored
+          audio={false}
+          screenshotFormat="image/jpeg"
+          ref={webcamRef}
+          className="w-full h-auto"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        {/* capture */}
+        {time && (
+        <p className="absolute inset-0 flex items-center justify-center text-white text-7xl bold">
+          {time == 1 ? "Smile! 📸" : time - 1}
+        </p>
+        )}
+      </div>
+
+      <button onClick={startCountdown} className="bg-blue-500 px-12 py-4 text-white rounded-lg cursor-pointer">Take a picture</button>
+      {imgSrc ? 
+        <Image alt="Webcam screenshot" src={imgSrc} width={500} height={500}/>
+        : "No image yet"} 
+{/* 
+      <Image src={strip} width={600} height={1800}/> */}
+      
     </div>
   );
 }
